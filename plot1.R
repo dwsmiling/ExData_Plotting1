1baa00eaeb80947d73dc8371c1b2ee5394f6be6d
dsmiling <dsmiling@gmail.com>
2014-05-11 11:20:09 -0700

    plot1

---
+++ plot1.R
@@ -0,0 +1,8 @@
+hpcraw <- read.csv("~/Documents/exdata-002/household_power_consumption.txt", sep=";”)
+
+hpcdt <- transform(subset(hpcraw[complete.cases(hpcraw),],Date == '1/2/2007' | Date == '2/2/2007'),DateTime = strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S”))
+
+hist(as.numeric(levels(hpcdt$Global_active_power)[levels(hpcdt$Global_active_power) != '?'])[hpcdt$Global_active_power],col="red",main="Global Active Power",xlab = "Global Active Power (kilowatts)”)
+
+dev.copy(png,"plot1.png”)
+dev.off()
